b1b4665dcee4260403e5b836ac054b7d2bbeaf8d
Lelouchlamperougexd <119339442+Lelouchlamperougexd@users.noreply.github.com>
2025-10-24 16:45:15 +0500

    gh p

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
   import path from 'path';
 
   export default defineConfig({
-    base: 'https://github.com/Lelouchlamperougexd/letter/',
+    base: '/letter/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
